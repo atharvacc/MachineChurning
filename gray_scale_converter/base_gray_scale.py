35ebee06623f1eea364ff08c2f2c93aa2f3b09b5
atharvacc <anchalke@ucdavis.edu>
2020-03-11 16:35:32 -0700

    Updating parser. Added stack images for testing parsing implementation

--- gray_scale_converter/base_gray_scale.py
+++ gray_scale_converter/base_gray_scale.py
@@ -17,6 +17,16 @@ from skimage.io import imread, imsave
 class base_gray_scale_model:
     """
     base_gray_scale_model: The basic gray scale model to be used for inference. The learning rate and image_directory is provided by user
+                Args: 
+                    (String) train_dir: directory where the training images can be found
+                    (int) lr          : learning rate
+                    (String) test_dir : directory where the testing images can be found
+                    (String) extension: extension of the images
+                    (int)   batch_size: batch size to be used for training 
+                    (int)   epochs    : number of epochs to train for 
+            (String) model_save_dir   : directory where the model should be saved after training 
+            (String) test_save_dir    : directory where we should save the results after running inference
+            (String) model_path       : path to pretrained model in h5 format.
 
     """
     def __init__(self, lr , batch_size, epochs, model_save_dir,  train_dir = None,  test_dir = None, extension = "png", model_path = None, test_save_dir = "/gray_results" ):
@@ -28,12 +38,20 @@ class base_gray_scale_model:
         self.epochs = epochs
         self.model_save_dir = model_save_dir
         self.test_save_dir = test_save_dir
-        if (model_path) == None:
+        if (model_path == None):
             self.model = self.initialize_model()
         else:
+            print("Using pretrained model\n")
             self.model = load_model(model_path)
 
     def initialize_model(self):
+        """
+        initialzie_model: initialize the model
+                Args:
+                    None    
+                Returns:
+                    model: new model
+        """
         model = Sequential()
         model.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape = (512,512,1)))
         model.add(Conv2D(64, (3, 3), activation='relu', padding='same', strides=2))
@@ -48,13 +66,22 @@ class base_gray_scale_model:
         model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
         model.add(UpSampling2D((2, 2)))
         model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-        model.add(Conv2D(3, (3, 3), activation='sigmoid', padding='same'))
+        model.add(Conv2D(3, (3, 3), activation='tanh', padding='same'))
         model.add(UpSampling2D((2, 2)))
         opt = Adam(learning_rate = self.lr)
         model.compile(optimizer=opt, loss='mae', metrics = ['mse'])
         return(model)
 
     def preprocess_data(self, directory, extension):
+        """
+        preprocess_data: 
+                    Args:
+                        directory: Directory that contain the images
+                        extension: extension of the images
+                    Returns:
+                        X_HE: numpy array containing the training data
+                        Y_HE: numpy array containing the output data (colored images)
+        """
         data, data_gray = load_from_dir(directory, extension)
         m,n,o = data_gray.shape
         X_HE = data_gray.reshape(m,n,o,1)
@@ -66,16 +93,18 @@ class base_gray_scale_model:
         validation_split = 0.1
         self.model.fit(x = X_train, y = y_train, batch_size=self.batch_size, epochs=self.epochs, validation_split=validation_split, verbose=1)
         if not os.path.exists(self.model_save_dir):
-            os.makedirs(self.model_save_dir)
-        self.model.save(model_save_dir + "./model/gray_scale/{}.hd5".format(datetime.now().strftime('%Y-%m-%d %H:%M')))
+            os.makedirs(self.model_save_dir + "/model/gray_scale/")
+        self.model.save(self.model_save_dir + "/model/gray_scale/model_{}.h5".format(datetime.now().strftime('%Y-%m-%d')))
 
     def predict(self):
         if not os.path.exists(self.test_save_dir):
             os.makedirs(self.test_save_dir)
         for img_name in os.listdir(self.test_dir):
-            img = imread(self.test_dir + "/" + img_name, as_gray=True).reshape(1,512,512,1)
-            pred = self.model.predict(img).reshape(512,512,3) * 255
-            pred = pred.astype(np.uint8)
-            imsave("{}/pred_{}".format(self.test_save_dir, img_name), pred)
+            if (img_name.endswith(self.extension)):
+                print("Working on img {} \n".format(self.test_dir + "/" + img_name))
+                img = imread(self.test_dir + "/" + img_name, as_gray=True).reshape(1,512,512,1)
+                pred = self.model.predict(img).reshape(512,512,3) * 255
+                pred = pred.astype(np.uint8)
+                imsave("{}/pred_{}".format(self.test_save_dir, img_name), pred)
         return  
 
