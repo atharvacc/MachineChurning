d81c23dea3420dff0f6166d0641c4a9bbdf97c61
atharvacc <anchalke@ucdavis.edu>
2020-03-13 03:55:07 -0700

    Adding cycle gan parser

--- main_parser.py
+++ main_parser.py
@@ -1,9 +1,8 @@
 import argparse
-
+import os
 from gray_scale_converter.base_gray_scale import base_gray_scale_model
 
 def add_training_mode_args(parser):
-    
     parser.add_argument("--lr", required=False, help = "learning_rate", default= 0.001, type = float)
     parser.add_argument("--train_dir", required=True, help = "Directory to find training data")
     parser.add_argument("--img_extension", required=False, help = "Extension for the images", default = "png", type = str)
@@ -20,6 +19,16 @@ def add_testing_mode_args(parser):
     parser.add_argument("--img_extension", required=False, help = "Extension for the images", default = "png", type = str)
     parser.add_argument("--test_save_dir", required=False, help = "Directory to save the results after inference", default = "./results")
     parser.add_argument("--model_path", required= True, help = "Full Path to the model to be used")
+
+def add_test_cycle_gan_args(parser):
+    parser.add_argument("--lr", required=False, help = "learning_rate", default= 0.001, type = float)
+    parser.add_argument("--train_dir", required=True, help = "Directory to find training data")
+    parser.add_argument("--batch_size", required=False, help = "batch_size to be used", default = 32, type = int)
+    parser.add_argument("--epochs", required=False, help = "Number of epochs", default= 80, type=int)
+    parser.add_argument("--epoch_decay", required=False, help = "Number of epochs for decaying after epochs", default= 20, type=int)
+    parser.add_argument("--model_save_dir", required=False, help = "Directory to save the model", default = "./saved_models")
+
+
 
 
 
@@ -43,13 +52,16 @@ def main():
             myModel.model.summary()
             myModel.predict()
             
-            """
+            
     elif (parser.parse_known_args()[0].model_name == "cycle-gan"):
         if (parser.parse_known_args()[0].train_or_test == "train"):
+            os.system("python ganilla/train.py --dataroot {train_dir} --lr {lr} --batch_size {batch_size} --niter {epochs} --niter_decay {epoch_decay} --checkpoints_dir {model_save_dir}  \
+            --loadSize 512 --fineSize 512 --display_winsize 512 --name test_cyclegan --model cycle_gan --netG resnet_fpn" .format(train_dir = parser.parse_args().train_dir),
+            lr = parser.parse_args().lr, batch_size = parser.parse_args().batch_size, model_save_dir = parser.parse_args().model_save_dir, epochs = parser.parse_args().epochs,
+             epoch_decay = parser.parse_args().epoch_decay)
             
         if (parser.parse_known_args()[0].train_or_test == "test"):
-            #Test param
-            """
+            #Test Param
 
                                             
 
