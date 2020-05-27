21111ead5df7ddd01c9d3cdb8671a578bd407d57
mgmagall <56101188+mgmagall@users.noreply.github.com>
2020-05-27 16:09:53 -0700

    Updated
    Saves all three RGB values now

--- Flask_test/tools/ColorMappingPixels.m
+++ Flask_test/tools/ColorMappingPixels.m
@@ -1,7 +1,7 @@
 clear;
 %Read in MUSE and H&E images
-MUSEimagepath = '/Users/moni/Desktop/images/Stack0001_real_A.png';
-HEimagepath = '/Users/moni/Desktop/images/Stack0001_fake_B.png';
+MUSEimagepath = '/Users/moni/Desktop/images/Stack0028_real_A.png';
+HEimagepath = '/Users/moni/Desktop/images/Stack0028_fake_B.png';
 [MUSEOriginalImage] = imread(MUSEimagepath);
 [HEImage] = imread(HEimagepath);
 
@@ -24,9 +24,21 @@ hold on;
 
 %select points from gray scaled images
 
-npoints = 50; %number of points being selected
-MUSEcoordinates = zeros(1,npoints);
-HEcoordinates = zeros(1,npoints);
+npoints = 25; %number of points being selected
+
+%MUSE_R = MUSEOriginalImage(:,:,1);
+%MUSE_G = MUSEOriginalImage(:,:,2);
+%MUSE_B = MUSEOriginalImage(:,:,3);
+%HEcoordinates = zeros(1,npoints);
+%HE_R = HEImage(:,:,1);
+%HE_G = HEImage(:,:,2);
+%HE_B = HEImage(:,:,3);
+
+museCordCol = zeros(1,npoints);
+museCordRow = zeros(1,npoints);
+heCordCol = zeros(1,npoints);
+heCordRow = zeros(1,npoints);
+
 color = {'b', 'g', 'r', 'c', 'm', 'y'};
 symbol = {'.','o','x','+', '*', 's', 'd', 'v'};
 
@@ -47,12 +59,16 @@ for idx = 1:npoints
     [X1,Y1] = getpts(ax1);
     plot(X1,Y1, string(currMarker));
     %fprintf('Muse point %d Value %d\n',idx, MUSEOriginalImage(floor(Y1),floor(X1)));
-    MUSEcoordinates(idx) = MUSEOriginalImage(floor(Y1),floor(X1));
+    %MUSEcoordinates(idx) = [MUSE_R(floor(Y1),floor(X1)), MUSE_G(floor(Y1),floor(X1)), MUSE_B(floor(Y1),floor(X1))];
+    museCordRow(idx) = X1;
+    museCordCol(idx) = Y1;
 
     [X2,Y2] = getpts(ax2);
     plot(X2,Y2, string(currMarker));
     %fprintf('HE point %d Value %d\n', idx, HEResultingImage(floor(Y2),floor(X2)));
-    HEcoordinates(idx) = HEImage(floor(Y2),floor(X2));
+    %HEcoordinates(idx) = [HE_R(floor(Y1),floor(X1)), HE_G(floor(Y1),floor(X1)), HE_B(floor(Y1),floor(X1))];
+    heCordRow(idx) = X2;
+    heCordCol(idx) = Y2;
 
     if(colorIndex == maxColors)
         colorIndex = 0;
@@ -66,14 +82,17 @@ end
 
 %disp(museImage);
 %disp(heImage);
+MUSErgb = impixel(MUSEOriginalImage, museCordCol, museCordRow);
+HErgb = impixel(HEImage, heCordCol, heCordRow);
 
 selectedPointDoc = append(stackNum,'_','selectedPnts','.csv');
 fileName = fullfile(pwd, string(selectedPointDoc));
 %open file
 fid = fopen(fileName, 'wt');
 %writing headers
-fprintf(fid, 'Muse, H&E\n');
+fprintf(fid, 'Muse_R, Muse_G, Muse_B, H&E_R, H&_G, H&E_B\n');
 %writing data
-fprintf(fid, '%f, %f\n',MUSEcoordinates, HEcoordinates);
+%fprintf(fid, '%f, %f\n',MUSEcoordinates, HEcoordinates);
+fprintf(fid, '%f %f %f, %f %f %f\n', MUSErgb, HErgb);
 %close file
 fclose(fid);
